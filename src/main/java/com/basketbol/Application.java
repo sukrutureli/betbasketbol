4804f832bc9d5b4fe1c3e9f929dd5722b10b701f
sukrut <sukrut@sukrut.ayesas.web>
2025-10-12 21:06:00 +0300

    dsds

--- src/main/java/com/basketbol/Application.java
+++ src/main/java/com/basketbol/Application.java
@@ -73,7 +73,7 @@ public class Application {
 
             List<PredictionResult> results = new ArrayList<>();
             for (Match m : matchStats) {
-                results.add(ensemble.predict(m, Optional.empty()));
+                results.add(ensemble.predict(m, Optional.of(m.getOdds())));
             }
 
             HtmlReportGenerator.generateHtml(matches, historyManager, matchStats, results, "basketbol.html");
