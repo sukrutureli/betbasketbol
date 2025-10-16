cde65db4048c2c56bf6b456d7e67e6aadcc26ae5
sukrut <sukrut@sukrut.ayesas.web>
2025-10-16 20:54:31 +0300

    dsadsad

--- src/main/java/com/basketbol/Application.java
+++ src/main/java/com/basketbol/Application.java
@@ -81,15 +81,14 @@ public class Application {
 				results.add(ensemble.predict(m, Optional.ofNullable(m.getOdds())));
 			}
 
-			HtmlReportGenerator.generateHtml(matches, historyManager, matchStats, results, "basketbol_.html");
+			HtmlReportGenerator.generateHtml(matches, historyManager, matchStats, results, "basketbol.html");
 			System.out.println("✅ basketbol.html oluşturuldu.");
 
 			LastPredictionManager lastPredictionManager = new LastPredictionManager(historyManager, results, matches);
 			lastPredictionManager.fillPredictions();
 
-			HtmlReportGenerator.generateHtmlForSublist(lastPredictionManager.getLastPrediction(),
-					"basketboltahmin.html");
-			System.out.println("basketboltahmin_.html oluşturuldu.");
+			HtmlReportGenerator.generateHtmlForSublist(lastPredictionManager.getLastPrediction(), "basketboltahmin.html");
+			System.out.println("basketboltahmin.html oluşturuldu.");
 
 		} catch (Exception e) {
 			System.out.println("GENEL HATA: " + e.getMessage());
