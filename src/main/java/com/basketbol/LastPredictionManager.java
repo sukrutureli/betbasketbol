894e09985f8a3d24dac8daf71805ff6e548e1642
Sukru Tureli <sukrutureli@gmail.com>
2025-11-03 14:24:24 +0300

    adasd

--- src/main/java/com/basketbol/LastPredictionManager.java
+++ src/main/java/com/basketbol/LastPredictionManager.java
@@ -1,5 +1,8 @@
 package com.basketbol;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,15 +31,15 @@ public class LastPredictionManager {
 	public void fillPredictions() {
 		for (int i = 0; i < historyManager.getTeamHistories().size(); i++) {
 			TeamMatchHistory th = historyManager.getTeamHistories().get(i);
-			
-			LastPrediction tempLastPrediction = new LastPrediction(matchInfo.get(i).getName(), matchInfo.get(i).getTime());
+
+			LastPrediction tempLastPrediction = new LastPrediction(matchInfo.get(i).getName(),
+					matchInfo.get(i).getTime());
 			String[] tahminListesi = { "MS1", "MS2", "Üst", "Alt" };
-			
+
 			tempLastPrediction.setScore(predictionResults.get(i).getScoreline());
 
 			for (String s : tahminListesi) {
-				if (calculatePrediction(th, predictionResults.get(i),
-						matchInfo.get(i), s) != null) {
+				if (calculatePrediction(th, predictionResults.get(i), matchInfo.get(i), s) != null) {
 					String t = s;
 					if (!s.startsWith("MS")) {
 						t = matchInfo.get(i).getOdds().gethOverUnderValue() + " " + s;
@@ -48,10 +51,11 @@ public class LastPredictionManager {
 
 			if (!tempLastPrediction.getPredictions().isEmpty()) {
 				lastPrediction.add(tempLastPrediction);
-				
+
 				String homeTeam = tempLastPrediction.getName().split("-")[0].trim();
 				String awayTeam = tempLastPrediction.getName().split("-")[1].trim();
-				PredictionData tempPredictionData = new PredictionData(homeTeam, awayTeam, tempLastPrediction.getPredictions());
+				PredictionData tempPredictionData = new PredictionData(homeTeam, awayTeam,
+						tempLastPrediction.getPredictions());
 				predictionData.add(tempPredictionData);
 			}
 		}
@@ -64,18 +68,18 @@ public class LastPredictionManager {
 		if (!h.isInfoEnough() && !h.isInfoEnoughWithoutRekabet()) {
 			return null;
 		}
-		
+
 		if (tahmin.equals("MS1")) {
 			percentageH = h.getMs1() * 100;
 			percentagePR = pr.getpHome() * 100;
-			if (matchInfo.getOdds().getMs1() > 0.0 && percentageH >= 55 && percentagePR >= 60
+			if (matchInfo.getOdds().getMs1() > 0.0 && isPercentageOK(percentageH, percentagePR)
 					&& isScoreOk(pr.getScoreline(), "MS1", matchInfo)) {
 				return "MS1";
 			}
 		} else if (tahmin.equals("MS2")) {
 			percentageH = h.getMs2() * 100;
 			percentagePR = pr.getpAway() * 100;
-			if (matchInfo.getOdds().getMs2() > 0.0 && percentageH >= 55 && percentagePR >= 60
+			if (matchInfo.getOdds().getMs2() > 0.0 && isPercentageOK(percentageH, percentagePR)
 					&& isScoreOk(pr.getScoreline(), "MS2", matchInfo)) {
 				return "MS2";
 			}
@@ -83,7 +87,7 @@ public class LastPredictionManager {
 			percentageH = h.getUst() * 100;
 			percentagePR = pr.getpOver25() * 100;
 			if (matchInfo.getOdds().getOver() > 0.0) {
-				if (percentageH >= 55 && percentagePR >= 60 && isScoreOk(pr.getScoreline(), "Üst", matchInfo)) {
+				if (isPercentageOK(percentageH, percentagePR) && isScoreOk(pr.getScoreline(), "Üst", matchInfo)) {
 					return "Üst";
 				}
 			}
@@ -91,7 +95,7 @@ public class LastPredictionManager {
 			percentageH = h.getAlt() * 100;
 			percentagePR = (1 - pr.getpOver25()) * 100;
 			if (matchInfo.getOdds().getUnder() > 0.0) {
-				if (percentageH >= 55 && percentagePR >= 60 && isScoreOk(pr.getScoreline(), "Alt", matchInfo)) {
+				if (isPercentageOK(percentageH, percentagePR) && isScoreOk(pr.getScoreline(), "Alt", matchInfo)) {
 					return "Alt";
 				}
 			}
@@ -117,14 +121,15 @@ public class LastPredictionManager {
 			return false;
 		}
 	}
-	
+
 	private String getOddsAndPercentage(String tahmin, MatchInfo match, PredictionResult pr) {
 		if (tahmin.equals("MS1")) {
 			return " (" + String.valueOf(match.getOdds().getMs1()) + " - %" + ((int) (pr.getpHome() * 100)) + ")";
 		} else if (tahmin.equals("MS2")) {
 			return " (" + String.valueOf(match.getOdds().getMs2()) + " - %" + ((int) (pr.getpAway() * 100)) + ")";
 		} else if (tahmin.equals("Alt")) {
-			return " (" + String.valueOf(match.getOdds().getUnder()) + " - %" + ((int) ((1 - pr.getpOver25()) * 100)) + ")";
+			return " (" + String.valueOf(match.getOdds().getUnder()) + " - %" + ((int) ((1 - pr.getpOver25()) * 100))
+					+ ")";
 		} else if (tahmin.equals("Üst")) {
 			return " (" + String.valueOf(match.getOdds().getOver()) + " - %" + ((int) (pr.getpOver25() * 100)) + ")";
 		}
@@ -139,5 +144,26 @@ public class LastPredictionManager {
 		return predictionData;
 	}
 
-}
+	public boolean isPercentageOK(double h, double pr) {
+		boolean result = false;
+		double hWd = 55;
+		double hWe = 60;
+		double prWd = 60;
+		double prWe = 65;
 
+		LocalDate now = LocalDate.now(ZoneId.of("Europe/Istanbul"));
+		boolean isWeekEnd = now.getDayOfWeek() == DayOfWeek.SATURDAY || now.getDayOfWeek() == DayOfWeek.SUNDAY;
+
+		if (isWeekEnd) {
+			if (h >= hWe && pr >= prWe) {
+				result = true;
+			}
+		} else {
+			if (h >= hWd && pr >= prWd) {
+				result = true;
+			}
+		}
+
+		return result;
+	}
+}
